1b57e8dd9e41eaa0668dd4e6caa85ba8df5dced6
AyberkKaanFilik <32483576+AyberkKaanFilik@users.noreply.github.com>
2018-01-26 22:08:24 +0300

    Arduino code is added

---
+++ ayberks.ino
@@ -0,0 +1,51 @@
+int pot = A0;
+int PWM =6;
+float DutyMapped=0;
+float Duty=0;
+        
+void setup() {
+  
+  pinMode(pot,INPUT);
+  pinMode(PWM,OUTPUT);
+  Serial.begin(115200);
+  setPwmFrequency(6, 8); //Sets the pwm output of 6 to nearly 7.8 kHz
+}
+
+void setPwmFrequency(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 64: mode = 0x03; break;
+      case 256: mode = 0x04; break;
+      case 1024: mode = 0x05; break;
+      default: return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 32: mode = 0x03; break;
+      case 64: mode = 0x04; break;
+      case 128: mode = 0x05; break;
+      case 256: mode = 0x06; break;
+      case 1024: mode = 0x07; break;
+      default: return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
+}
+
+void loop() {                
+                Duty=analogRead(ayar);  
+                DutyMapped = map(D,0,1023,25,230);
+                analogWrite(PWM,DutyMapped);
+                delay(10);
+             
+        }
